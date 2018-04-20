bb4a30b5ad6ce2fab6e8b7a58d952a97b3c09d3b
Jonathan Bertoldi <jonathanbertoldi7@gmail.com>
2018-04-20 16:18:09 -0300

    Estrutura de pastas e projetos

---
+++ Tully.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tully.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tully.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tully.Data
+{
+    public class Class1
+    {
+    }
+}
